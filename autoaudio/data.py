302cb5583b741b34861e634091814e86eb24739b
Archy de Berker <archy.deberker@gmail.com>
2018-07-08 17:14:39 -0400

    Update AudioCommandDataset to accept pre-processed audio files

--- autoaudio/data.py
+++ autoaudio/data.py
@@ -2,27 +2,26 @@ import glob
 import os
 
 import numpy as np
-import scipy.io.wavfile as wav
-from keras.preprocessing.sequence import pad_sequences
 
 from autoaudio.utils import audio
 
 
 class AudioCommandDataset():
+    """ Class for handling pre-processed audio command datasets.
 
-    def __init__(self, data_path, batch_size=16, output_size=16000):
+    By default, loads mel spectrograms."""
+
+    def __init__(self, data_path, batch_size=16):
 
         self.data_path = data_path
         self.batch_size = batch_size
-        self.file_list = get_filelist(self.data_path)
+
+        self.file_list = get_filelist(self.data_path, suffix='_mel.npy')
 
         self.val_set = self._get_file_paths(text_file='validation_list.txt')
         self.test_set = self._get_file_paths(text_file='testing_list.txt')
 
         self.train_set = self._get_train_paths()
-
-        self.output_size = output_size
-
 
     def _get_train_paths(self):
         excluded = self.val_set.union(self.test_set)
@@ -33,44 +32,48 @@ class AudioCommandDataset():
             file_list = f.readlines()
 
         file_list = [os.path.join(self.data_path, f).rstrip() for f in file_list]
+
+        # Strip file suffixes to allow us to compare these lists to pre-processed data
+        file_list = [self._strip_suffix(f) for f in file_list]
+
         return set(file_list)
+
+    @staticmethod
+    def _strip_suffix(path):
+        """ Strips the suffixes introduced by pre-processing, to allow us to compare
+        file paths to the validation and test set lists.
+        """
+
+        suffixes = ['.wav', '.npy', 'spec', 'mel']
+
+        path_components = path.split('_')
+
+        return '_'.join([p for p in path_components if p not in suffixes])
 
     def _random_filename(self, path_set):
 
-        file_name = np.random.choice(self.file_list)
-        print(file_name)
+        file_name = self._strip_suffix(np.random.choice(self.file_list))
         while file_name not in path_set:
-            file_name = np.random.choice(self.file_list)
+            file_name = self._strip_suffix(np.random.choice(self.file_list))
 
         return file_name
-
-    @staticmethod
-    def load_audio(audio_file):
-
-        _, data = wav.read(audio_file)
-
-        return data
-
-    def _preprocess_audio_batch(self, audio_batch):
-
-        return pad_sequences(audio_batch, maxlen=self.output_size)
 
     def get_batch(self, path_set):
 
         while True:
             x = []
             for i in range(self.batch_size):
-                x.append(self.load_audio(self._random_filename(path_set)))
+                x.append(np.load(self._random_filename(path_set)))
 
-            yield self._preprocess_audio_batch(np.asarray(x))
+            yield np.asarray(x)
 
 
-def get_filelist(data_path):
-
+def get_filelist(data_path, suffix='.wav'):
+    """ Retrieve a list of all files in the speech_commands dataset hierarchy."""
     file_list = []
     for folder in os.listdir(data_path):
         if os.path.isdir(os.path.join(data_path, folder)) and '_background_noise_' not in folder:
-            for file in glob.glob(os.path.join(data_path, folder, '*.wav')):
+            for file in glob.glob(os.path.join(data_path, folder, '*'+suffix)):
 
                 file_list.append(os.path.join(data_path, folder, file))
 
@@ -78,6 +81,8 @@ def get_filelist(data_path):
 
 
 def process_utterance(out_dir, wav_path):
+
+    """Convert an audio clip into spectogram and mel spectrogram."""
 
     wav = audio.load_wav(wav_path)
 
